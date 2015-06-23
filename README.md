8f95dec637b9ef15d7ebb4299df86ac3828f370b
Javier Aladid <lechavs@gmail.com>
2015-06-23 11:45:38 +0200

    added retry policy, and keep alive setting,bump version

--- README.md
+++ README.md
@@ -4,19 +4,27 @@ sshPlugin
 A grails plugin to perform ssh and scp. Based on http://www.ganymed.ethz.ch/ssh2/
 
 Instantiate the sshClient:
+```
 sshClient client = new SshClient( host: "0.0.0.0", user: "root", port: 22, password: "changeme" )
-
+```
 If password is provided password will be used to auth, if not and sshkey provided, sshkey will be used:
+```
 sshClient client = new SshClient( host: "0.0.0.0", user: "root", port: 22, sshkey: "~/.ssh/id_rsa", sshkeypass:"" )
+```
+Also the client can be configured with 
+* keepAlive (boolean) : will keep the connection alive betweeen calls to either execute() or put/get
+* retries ( int ) : amount of times the client will try to connect
+* retryInterval ( int ): miliseconds between retries
 
 Once the client is configured, we can call execute, get or put
-
+```
 SshResponse resp = client.execute("echo Hola")
 println "STDOUT: ${resp.out}"
 println "STDERROR: ${resp.error}
 println "EXIT STATUS ${resp.exit}"
 
-client.get("~/remotefile", "~/localFile")
+client.getFile("~/remotefile", "~/localFile")
 
-client.put("~/localFile", "~/remotefile")
-
+client.putFile("~/localFile", "~/remotefile")
+client.putFile( "hello world" as byte[], "myfilename", "/tmp/"  )
+```
--- src/groovy/grails/plugin/ssh/SshClient.groovy
+++ src/groovy/grails/plugin/ssh/SshClient.groovy
@@ -15,11 +15,24 @@ class SshClient{
   String sshkey
   String sshkeypass
 
+  boolean keepAlive
+  Connection connection
+
+  Integer retries = 5
+  Long retryInterval = 1000 // miliseconds
+
 
   SshResponse execute( String cmd ) throws InterruptedException {
     SshResponse response
-    def connection = getConnection()
-    Session session = connection.openSession()
+    connection = getConnection()
+    Session session
+    try{
+      session = connection.openSession()
+    }catch( e ){
+      log.error "error connecting, retrying", e
+      connection = getConnection( true )
+      session = connection.openSession()
+    }
     LOG.info "Executing in $host [ $cmd ]"
     try{
       session.execCommand("$cmd")
@@ -31,50 +44,95 @@ class SshClient{
       return response
     } finally {
       session.close()
-      connection.close()
+      if (!keepAlive) {
+        connection.close()
+        this.connection = null
+      }
     }
   }
 
   void getFile( String sourcePath, String targetPath ){
     def connection = getConnection()
-    SCPClient scp = connection.createSCPClient()
+    SCPClient scp
+    try{
+      scp = connection.createSCPClient()
+    }catch( e ){
+      log.error "error connecting, retrying", e
+      connection = getConnection( true )
+      scp = connection.createSCPClient()
+    }
     if ( LOG.debugEnabled )
       LOG.debug "Getting file from $host $sourcePath to local $targetPath"
     try{
       scp.get( sourcePath, targetPath )
     }finally {
-      connection.close()
+      if (!keepAlive) {
+        connection.close()
+        this.connection = null
+      }
     }
   }
 
   void putFile(  String sourcePath, String targetPath  ){
     def connection = getConnection()
-    SCPClient scp = connection.createSCPClient()
+    SCPClient scp
+    try{
+      scp = connection.createSCPClient()
+    }catch( e ){
+      log.error "error connecting, retrying", e
+      connection = getConnection( true )
+      scp = connection.createSCPClient()
+    }
     if ( LOG.debugEnabled )
       LOG.debug "Putting file local $sourcePath to $host $targetPath"
     try{
       scp.put( sourcePath, targetPath )
     }finally {
-      connection.close()
+      if (!keepAlive) {
+        connection.close()
+        this.connection = null
+      }
     }
   }
 
   void putFile(  byte[] data, String name, String targetPath  ){
     def connection = getConnection()
-    SCPClient scp = connection.createSCPClient()
+    SCPClient scp
+    try{
+      scp = connection.createSCPClient()
+    }catch( e ){
+      log.error "error connecting, retrying", e
+      connection = getConnection( true )
+      scp = connection.createSCPClient()
+    }
     if ( LOG.debugEnabled )
       LOG.debug "Putting file to $host $targetPath / $name"
     try{
       scp.put( data, name, targetPath )
     }finally {
-      connection.close()
+      if (!keepAlive) {
+        connection.close()
+        this.connection = null
+      }
     }
   }
 
-  private getConnection(){
+  private getConnection( boolean force = false ){
+    if (connection && !force) return connection
+
     File keyfile = sshkey? new File( sshkey ) : null
-    Connection connection = new Connection( host, port ?: 22 )
-    connection.connect()
+    Connection connection
+    for ( int i = 0 ; i < retries ; i++ ){
+      try{
+        LOG.info "Connecting attempt $i / $retries"
+        connection = new Connection( host, port ?: 22 )
+        connection.connect()
+      }catch( e ){
+        LOG.warn "Error connecting to $host $port"
+        if ( i < retries ) Thread.sleep( retryInterval )
+        else throw e
+      }
+    }
     boolean authenticated = false
     if ( keyfile ) { //keyfile always preferred
       if ( LOG.debugEnabled )
